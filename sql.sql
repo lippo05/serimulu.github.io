091e98994a9ff2331fff0592daa809ef4e27cca5
lippo05 <maliffmenasik746@gmail.com>
2025-04-19 23:09:41 +0800

    Delete sql.sql

--- sql.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE parcels (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  customer_name VARCHAR(255),
-  parcel_id VARCHAR(100),
-  status VARCHAR(50),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
